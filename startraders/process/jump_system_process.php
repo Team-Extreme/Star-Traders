f571ce02f81b8597392399674568952c7d5d7ff6
DaveDavisDavidson <60824118+DaveDavisDavidson@users.noreply.github.com>
2020-02-26 17:33:52 +0000

    Add files via upload

--- startraders/process/jump_system_process.php
+++ startraders/process/jump_system_process.php
@@ -4,6 +4,7 @@
 
         require '../php/db_connect.php';
         $system=$_GET['system_id'];
+        $fuel_cost=$_GET['system_id'];
 
         $sql = "SELECT MIN(planet_id)  FROM univ_planet where system_id ='".$_GET['system_id']."'";
        
@@ -19,8 +20,15 @@
 		
 		$player_x =$sql_array["system_x"];
 		$player_y =$sql_array["system_y"];
-        
 
+        $sql = "SELECT ship_fuel FROM user_ship where user_id ='".$_SESSION['user_id']."'";
+       
+        $result = $conn->query($sql);
+        $sql_array = $result->fetch_array(MYSQLI_ASSOC);
+		
+		$ship_fuel =$sql_array["ship_fuel"];
+        if ($ship_fuel >= $fuel_cost){
+            $ship_fuel_update= $ship_fuel - $fuel_cost
         $sql = "UPDATE user_table SET user_location ='".$_GET['system_id']."' WHERE user_id='".$_SESSION['user_id']."'";
 
         $results =$conn->query($sql);
@@ -32,7 +40,15 @@
         $results =$conn->query($sql);
 		        $sql = "UPDATE user_table SET user_y='".$player_y."' WHERE user_id='".$_SESSION['user_id']."'";
         $results =$conn->query($sql);
-
+        $sql = "UPDATE user_ship SET ship_fuel ='".$ship_fuel_update."' WHERE user_id='".$_SESSION['user_id']."'";
+        $results =$conn->query($sql);
         header('Location:../php/map.php?message_id=4');
-
+            
+            
+        
+}
+else{
+            header('Location:../php/map.php?message_id=8');
+    
+}
 ?>
