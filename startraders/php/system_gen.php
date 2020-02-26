bda5542e9b56f2d92eff05dcc7ebe2d458993bb8
DaveDavisDavidson <60824118+DaveDavisDavidson@users.noreply.github.com>
2020-02-26 18:37:27 +0000

    Add files via upload
    Star Class is now selected from an array of possible star classifications.
    $star_classification_array = array("O","B","A","F","G","K","M");
    This can be used in the future to classify planets based on proximity to their star

--- startraders/php/system_gen.php
+++ startraders/php/system_gen.php
@@ -6,6 +6,8 @@
 					//create system
         $system_array = array("Andromeda", "Aries", "Ajax", "Apollo", "Castor", "Daemon", "Hermes", "Theseus","Caldron", "Aquilla", "Proteus", "Atlas", "Ailea", "Black", "Unathea", "Aopra","Taloth", "Eria", "Mapra", "Naviri", "Haonus", "Kurus", "Aurus", "Diria","Asypso", "Velosia", "Gek", "Gert", "Rune", "Obion", "Freya", "Runifile","Scarrow", "Laibach", "Green", "Pinus", "Velos", "Melodrus", "Frarn", "Hek","Filus", "Bern", "Cern", "Velum", "Sargon", "New Sol", "Grup", "Bleo","Farse", "Rileum", "Astrus", "Nevor", "Felum", "Grak", "Teitov", "Zenom","Felnoe", "Ovecury", "Xoalea", "Chion", "Oupra", "Zuwei", "Saelara", "Chalveon","Perlos", "Deogawa", "Uchoth", "Obtalium", "Vega", "Zeta", "Alpha", "Beta","Gamma", "Theta", "Omega", "Vapra", "Bespin", "Vespa", "Igeon", "Ratheon","Celcius", "Musk", "Hel", "Coliv", "Blerp", "Cacus", "Bismuth", "Barundi","Thonov", "Valkov", "Antov", "Tovis", "Rebus", "Atune", "Leviturn", "Nunia","Nunes", "Siicury", "Kolvone", "Atania", "Saowei", "Leotau", "Odius", "Opeas","Biziri", "Niburu", "Caltron", "Grego", "Vaz", "Ne'Sol", "Grut","Terridine","Velzia", "Nazila", "Cerberus", "Zeras", "Umbrea", "Fen", "Ferelas","Stormwind","Hisav", "Avote", "Newas", "Menva", "Nerva", "Uri", "Garga","Nehov","Nabia", "Hetoo", "Maguda", "Menios", "Khufu", "Temple", "Sanctum","Cheops","Knara", "Necho", "Neto", "Vei", "Zei", "Zuc", "Halcroyn","Merlin","Prince", "Cheff", "Madriga", "Menilous", "Zuccu", "Zamu", "Ballobi");
             
+        $star_classification_array = array("O","B","A","F","G","K","M");
+        $randIndex = array_rand($star_classification_array);
         $system_x =rand(300,4500);
         $system_y =rand(300,2500);
         $system_description= "THIS IS A SYSTEM";
@@ -14,10 +16,8 @@
 		$system_security=rand(1,10);
         $star_x=120;
         $star_y=120;
-        $star_class =rand(1,5);
-            
+        $star_class = $star_classification_array[$randIndex];
           $sql = "INSERT INTO univ_star(system_id,star_name,star_x,star_y,star_class) VALUES ('$system_id','$system_name','$star_x','$star_y','$star_class')";
-				echo  $sql;
             $conn->query($sql);
             
 
